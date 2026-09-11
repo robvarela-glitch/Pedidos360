7ff9059da5330d5cfd7c3e0ac0bdc81d46f8d310
robvarela-glitch <rob.varela@duocuc.cl>
2026-09-11 12:38:48 -0300

    Conectar vista de catalogo con servicio

--- frontend-pedidos360/src/app/core/services/catalog.service.ts
+++ frontend-pedidos360/src/app/core/services/catalog.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 import { Product } from '../models/product.model';
 
@@ -14,7 +14,43 @@ export class CatalogService {
   constructor(private http: HttpClient) {}
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.apiUrl);
+
+    const mockProducts: Product[] = [
+      {
+        id: 1,
+        name: 'Notebook Lenovo',
+        sku: 'SKU-001',
+        category: 'Tecnología',
+        stock: 15,
+        price: 599990
+      },
+      {
+        id: 2,
+        name: 'Mouse Logitech',
+        sku: 'SKU-002',
+        category: 'Accesorios',
+        stock: 42,
+        price: 19990
+      },
+      {
+        id: 3,
+        name: 'Teclado Mecánico',
+        sku: 'SKU-003',
+        category: 'Accesorios',
+        stock: 4,
+        price: 39990
+      },
+      {
+        id: 4,
+        name: 'Smartphone Samsung',
+        sku: 'SKU-004',
+        category: 'Telefonía',
+        stock: 21,
+        price: 399990
+      }
+    ];
+
+    return of(mockProducts);
   }
 
   getProductById(id: number): Observable<Product> {
--- frontend-pedidos360/src/app/pages/catalog/catalog.ts
+++ frontend-pedidos360/src/app/pages/catalog/catalog.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Product } from '../../core/models/product.model';
+import { CatalogService } from '../../core/services/catalog.service';
 
 @Component({
   selector: 'app-catalog',
@@ -6,6 +9,24 @@ import { Component } from '@angular/core';
   templateUrl: './catalog.html',
   styleUrl: './catalog.css'
 })
-export class Catalog {
+export class Catalog implements OnInit {
 
+  products: Product[] = [];
+
+  constructor(private catalogService: CatalogService) {}
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    this.catalogService.getProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+      },
+      error: (error) => {
+        console.error('Error al cargar los productos:', error);
+      }
+    });
+  }
 }
