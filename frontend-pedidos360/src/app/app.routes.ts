a36f7327ffd1087a9adab878fbd56779ddd568db
robvarela-glitch <rob.varela@duocuc.cl>
2026-09-11 12:23:44 -0300

    Crear vistas principales del frontend

--- frontend-pedidos360/src/app/app.routes.ts
+++ frontend-pedidos360/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Catalog } from './pages/catalog/catalog';
+import { Orders } from './pages/orders/orders';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard
+  },
+  {
+    path: 'catalog',
+    component: Catalog
+  },
+  {
+    path: 'orders',
+    component: Orders
+  }
+];
